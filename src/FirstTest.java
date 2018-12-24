b949d7637651d2c681bf8a2e3960faa7cb5e7cfd
svitlana.shepotilova <svitlana.shepotilova@gmail.com>
2018-12-24 15:49:36 +0100

    ex2 Find text "Search..."

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -3,13 +3,20 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertTrue;
 
 public class FirstTest {
 
-    public AppiumDriver driver;
+    private AppiumDriver driver;
 
     @Before
     public void setUp() throws Exception {
@@ -23,6 +30,7 @@ public class FirstTest {
         capabilities.setCapability("app", "C:\\Users\\svitlana.shepotilova.PS-1142-PC\\AndroidStudioProjects\\JavaAppiumAutomation\\apks\\org.wikipedia.apk");
 
         driver = new AndroidDriver(new URL("http://127.00.1:4723/wd/hub"), capabilities);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
     }
 
     @After
@@ -30,8 +38,34 @@ public class FirstTest {
         driver.quit();
     }
 
+
     @Test
-    public void firstTest() {
-        System.out.println("first test run!");
+    public void ex2FindTextSearch() {
+        waitForElementPresentByAndClick(
+                By.className("android.widget.TextView"),
+                "Text element not found",
+                5);
+        WebElement element_to_send_string = waitForElementPresentBy(
+                By.className("android.widget.EditText"),
+                "Edit text input not found",
+                5);
+        assertTrue("Text not found ",
+                element_to_send_string.getAttribute("text").equals("Search…"));
+        assertTrue("This assert should fail because text not present on the page",
+                element_to_send_string.getAttribute("text").equals("1Search…"));
+
     }
+
+    private WebElement waitForElementPresentBy(By by, String errorMessage, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(errorMessage);
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+    private WebElement waitForElementPresentByAndClick(By by, String errorMessage, long timeoutInSeconds) {
+        WebElement element = waitForElementPresentBy(by, errorMessage, timeoutInSeconds);
+        element.click();
+        return element;
+    }
+
 }
