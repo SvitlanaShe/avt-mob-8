b8c6e0f2a2babc94b752ad78a9221b0d9967759c
svitlana.shepotilova <svitlana.shepotilova@gmail.com>
2018-12-24 16:00:08 +0100

    ex3 Search the word, check results present and contains the word

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -3,13 +3,19 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class FirstTest {
 
-    public AppiumDriver driver;
+    private AppiumDriver driver;
 
     @Before
     public void setUp() throws Exception {
@@ -23,6 +29,7 @@ public class FirstTest {
         capabilities.setCapability("app", "C:\\Users\\svitlana.shepotilova.PS-1142-PC\\AndroidStudioProjects\\JavaAppiumAutomation\\apks\\org.wikipedia.apk");
 
         driver = new AndroidDriver(new URL("http://127.00.1:4723/wd/hub"), capabilities);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
     }
 
     @After
@@ -31,7 +38,43 @@ public class FirstTest {
     }
 
     @Test
-    public void firstTest() {
-        System.out.println("first test run!");
+    public void ex4VerifyTextPresentInSearchResultsList() {
+        String text = "Welt";
+        By by = By.xpath("//*[@text='Search and read the free encyclopedia in your language']");
+        By listOfElementsBy = By.xpath("//android.widget.TextView[contains(@text,'" + text + "')]");
+
+        waitForElementPresentByAndClick(
+                By.className("android.widget.TextView"),
+                "Text element not found",
+                5);
+        waitForElementPresentBy(by, "Text not found.", 5);
+        waitForElementPresentByAndSendKeys(
+                By.className("android.widget.EditText"),
+                text,
+                "Edit text input not found",
+                5);
+        waitForElementPresentBy(listOfElementsBy, "No results for search found", 5);
+        List<WebElement> listOfElements = driver.findElements(listOfElementsBy);
+
+        listOfElements.forEach((searchResult) -> System.out.println(searchResult.getText() + "; "));
+
+    }
+
+    private WebElement waitForElementPresentBy(By by, String errorMessage, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(errorMessage);
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+    private WebElement waitForElementPresentByAndClick(By by, String errorMessage, long timeoutInSeconds) {
+        WebElement element = waitForElementPresentBy(by, errorMessage, timeoutInSeconds);
+        element.click();
+        return element;
+    }
+
+    private WebElement waitForElementPresentByAndSendKeys(By by, String keys, String errorMessage, long timeoutInSeconds) {
+        WebElement element = waitForElementPresentBy(by, errorMessage, timeoutInSeconds);
+        element.sendKeys(keys);
+        return element;
     }
 }
