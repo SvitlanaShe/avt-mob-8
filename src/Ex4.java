5ffdbf35a3571da325760e831fda59db569f906a
svitlana.shepotilova <svitlana.shepotilova@gmail.com>
2018-12-28 22:13:23 +0100

    lesson 3 , tests from lesson

--- src/Ex4.java
+++ src/Ex4.java
@@ -1,10 +1,12 @@
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -14,7 +16,7 @@ import java.net.URL;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class FirstTest {
+public class Ex4 {
 
     private AppiumDriver driver;
 
@@ -79,4 +81,5 @@ public class FirstTest {
         element.sendKeys(keys);
         return element;
     }
+
 }
